935a41728b0d780f6d78f7356f7869a9d4272fea
twok1 <twokey25@gmail.com>
2025-10-27 14:21:45 +0300

    reminder adv

--- src/application.py
+++ src/application.py
@@ -1,6 +1,7 @@
 from src.command_trip_processor import CommandTripProcessor
 from src.email_parser import EmailParser
 from src.email_reader import EmailReader
+from src.reminder_calculator import ReminderCalculator
 
 
 class Application:
@@ -8,10 +9,11 @@ class Application:
         self.mail_reader = EmailReader()
         self.mail_parser = EmailParser()
         self.command_trip_processor = CommandTripProcessor()
+        self.remind_calculator = ReminderCalculator()
     
     def run(self):
         messages = self.mail_reader.run()
         parsed_messages = self.mail_parser.run(messages)
         processed_messages = self.command_trip_processor.run(parsed_messages)
-        for msg in processed_messages:
-            print(msg.start_date, msg.end_date)
+        self.remind_calculator.run(processed_messages)
+        
--- src/reminder_calculator.py
+++ src/reminder_calculator.py
@@ -1,8 +1,23 @@
-from src.models import Reminder
+import configparser
+import os
+from typing import List
 
-class ReinderCalculator:
+from src.models import Reminder
+from src.models.models import CommandTrip
+
+class ReminderCalculator:
+    SCRIPT_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config_mails.ini'))
+
+    config = configparser.ConfigParser()
+    config.read(SCRIPT_PATH, encoding='utf-8')
+    FUCKING_OUT =  (int(config.get('mails', 'START_LOCK')), int(config.get('mails', 'END_LOCK')))
+    START_NOTIFICATION = list([i for i in map(int, config.get('mails', 'START_NOTIFICATION').split(', '))])
+    END_NOTIFICATION = list([i for i in map(int, config.get('mails', 'END_NOTIFICATION').split(', '))])
+    WORKING_RANGE = tuple([i for i in map(int, config.get('mails', 'WORKING_RANGE').split(', '))])
     def __init__(self) -> None:
         pass
     
-    def run(self):
-        pass
+    def run(self, messages: List[CommandTrip]) -> List[Reminder]:
+        reminders: List[Reminder] = []
+        
+        return reminders
