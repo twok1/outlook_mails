654cb828aa4bb9b339e816037c8ad48134bc79a7
twok1 <twokey25@gmail.com>
2025-10-27 16:31:56 +0300

    add models for db

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,11 @@
 from .dataclasses import EmailData, CommandTrip, Reminder
+from .database_models import CommandTripDB, ReminderDB, Base
 
 __all__ = [
     'EmailData',
     'CommandTrip',
-    'Reminder'
+    'Reminder',
+    'CommandTripDB',
+    'ReminderDB',
+    'Base'
 ]
