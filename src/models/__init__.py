dd329d607ba171718488a2161fe070195dbc91b3
twok1 <twokey25@gmail.com>
2025-12-02 16:08:03 +0300

    rem sqlalchemy

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,7 @@
 from .dataclasses import EmailData, CommandTrip, Reminder
-from .database_models import CommandTripDB, ReminderDB, Base
 
 __all__ = [
     'EmailData',
     'CommandTrip',
     'Reminder',
-    'CommandTripDB',
-    'ReminderDB',
-    'Base'
 ]
