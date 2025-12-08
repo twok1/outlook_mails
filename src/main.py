ee94f25b2f0809e1697f670ff946f9ce547474c9
twok1 <twokey25@gmail.com>
2025-12-08 10:22:39 +0300

    срипт делает проверку писем каждый час после запуска

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-import time
+import schedule
+
 from .application import Application
 
+def create():
+    try:
+        app = Application()
+        app.run()
+    except:
+        pass
+    del(app)
+
 def main():
-    app = Application()
-    app.run()
-    time.sleep(900)
-    app.run()
+    create()
+    schedule.every().hour.at('00').do(create)
+    while True:
+        schedule.run_pending()
