9f621e54e942b4468d97e6ac71febcb210f5e35a
twok1 <twokey25@gmail.com>
2025-12-08 10:12:51 +0300

    перевожу проект на exchangelib. чтобы не зависеть от запуска outlook TODO: необходимо отладить парсинг письма в модель EmailData

--- src/email_reader.py
+++ src/email_reader.py
@@ -1,29 +1,49 @@
+import os
 from typing import List
-import win32com.client
+import sertifi
 
+from exchangelib import Credentials, Configuration, Account, DELEGATE, Q
 from .models import EmailData
 from .filters import ClassFilter, SubjectFilter, SenderEmailFilter
 
 class EmailReader:
-    FILTERS = (
-        ClassFilter(52),
-        SubjectFilter(
-            'Информирование о направлении в командировку', 
-            'Информирование об изменении данных командировки'
-        ),
-        SenderEmailFilter('iasup_notify@greenatom.ru')
-    )
-    OUTLOOK_INBOX = 6
+    def __init__(self):
+        self.outlook = self.connect_to_exchange()
     
-    def __init__(self, folder: int=OUTLOOK_INBOX) -> None: # 6- папка Входящие Outlook
-        self.folder = folder
-        self.outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
-        
     def run(self):
-        messages = self.outlook.GetDefaultFolder(self.folder).Items
-        for filter in self.FILTERS:
-            messages = [msg for msg in messages if filter.apply(msg)]
+        messages = self.outlook.inbox.filter(
+            sender__email_adress_exact='iasup_notify@greenatom.ru'
+        ).filter(
+            Q(subject__contains='Информирование о направлении в командировку') |
+            Q(subject__contains='Информирование об изменении данных командировки')
+        )
         return self._process_to_emaildata(messages=messages)
+    
+    def connect_to_exchange(self):
+        """Подключение к Exchange."""
+        EMAIL = os.getenv('EMAIL')
+        PASSWORD = os.getenv('PASS')
+        SERVER = os.getenv('SERV')
+        
+        credentials = Credentials(username=EMAIL, password=PASSWORD)
+
+        # print("Подключение без SSL проверки")
+        # BaseProtocol.HTTP_ADAPTER_CLS = NoVerifyHTTPAdapter
+        
+        config = Configuration(
+            server=SERVER,
+            credentials=credentials,
+            verify_ssl=sertifi.where(),
+        )
+        
+        account = Account(
+            primary_smtp_address=EMAIL,
+            config=config,
+            autodiscover=False,
+            access_type=DELEGATE
+        )
+        
+        return account
     
     def _process_to_emaildata(self, messages: list) -> List[EmailData]:
         new_messages = []
