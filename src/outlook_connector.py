7599b28b431ce2b5ede6687ee0483d0b021baa53
twok1 <twokey25@gmail.com>
2026-01-20 16:49:41 +0300

    добавил смену директории

--- src/outlook_connector.py
+++ src/outlook_connector.py
@@ -1,8 +1,20 @@
 import os
 import warnings
+import sys
 
 from exchangelib import BaseProtocol, Credentials, Configuration, Account, DELEGATE, NoVerifyHTTPAdapter
 from dotenv import load_dotenv
+
+# Получаем путь к директории, где находится исполняемый файл
+if getattr(sys, 'frozen', False):
+    # Если запускается как exe-файл
+    app_path = os.path.dirname(sys.executable)
+else:
+    # Если запускается как Python-скрипт
+    app_path = os.path.dirname(os.path.abspath(__file__))
+
+# Устанавливаем рабочую директорию
+os.chdir(app_path)
 
 load_dotenv()
 warnings.filterwarnings('ignore')
